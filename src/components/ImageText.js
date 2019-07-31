605b90fea167adbb48e8c030adeb156337b7a48e
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-07-30 21:38:16 -0400

    added custom scrollbars to explanation text

--- src/components/ImageText.js
+++ src/components/ImageText.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Scrollbar } from "react-scrollbars-custom";
 import "./imageText.css";
 //import HDButton from "./HDButton"
 
@@ -7,7 +8,9 @@ const ImageText = ({ title, exp }) => {
     <div className="main-text-div">
       <h3>{title}</h3>
       <div className="inner-text-div">
-        <p>{exp}</p>
+        <Scrollbar style={{ width: "100%", height: "100%" }}>
+          <p>{exp}</p>
+        </Scrollbar>
       </div>
     </div>
   );
